27f7794a473c3c5697f8899a4db43d49a1931e77
Nataly <63913225+Nataly-li@users.noreply.github.com>
2021-04-23 00:54:01 +0300

    Исправила опечатки и удалила ненужную строку с display = block

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ fetch("https://api.github.com/users/Nataly-li")
     document.body.appendChild(divBio);
     let img = new Image(250, 250);
     img.src = json.avatar_url;
-    img.style.display = "block";
     document.body.appendChild(img);
   })
-  .catch((err) => console.log("Информация о пользователе не надена."));
+  .catch((err) => console.log("Информация о пользователе не найдена."));
