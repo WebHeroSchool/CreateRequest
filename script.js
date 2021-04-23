36b0b7219dc385066b47a566a92c06be284c406e
Nataly <63913225+Nataly-li@users.noreply.github.com>
2021-04-23 16:41:03 +0300

    Получила username, сделала проверку на пустую строку и вывод данных

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 fetch("https://api.github.com/users/Nataly-li")
   .then((res) => res.json())
   .then((json) => {
-    let nameLink = document.createElement("a");
-    nameLink.id = "link";
-    nameLink.href = json.html_url;
-    nameLink.title = "Link";
-    nameLink.innerHTML = "Nataly-li";
-    document.body.appendChild(nameLink);
+    try {
+      let userName = window.location.toString().split()[0].slice(49, 58);
 
-    let divBio = document.createElement("div");
-    divBio.id = "bio";
-    if (json.bio === null) {
-      divBio.innerHTML = "Every cloud has a silver lining.";
+      let nameLink = document.createElement("a");
+      nameLink.id = "link";
+      nameLink.href = json.html_url;
+      nameLink.title = "Link";
+      nameLink.innerHTML = userName;
+      document.body.appendChild(nameLink);
+
+      if (!userName || userName == "") {
+        nameLink.innerHTML = "Natalie";
+      }
+
+      let divBio = document.createElement("div");
+      divBio.id = "bio";
+      if (json.bio === null) {
+        divBio.innerHTML = "Every cloud has a silver lining.";
+      }
+
+      document.body.appendChild(divBio);
+      let img = new Image(250, 250);
+      img.src = json.avatar_url;
+      document.body.appendChild(img);
+    } catch (err) {
+      document.body.innerHTML = "Информация о пользователе не найдена.";
     }
-
-    document.body.appendChild(divBio);
-    let img = new Image(250, 250);
-    img.src = json.avatar_url;
-    document.body.appendChild(img);
-  })
-  .catch((err) => console.log("Информация о пользователе не найдена."));
+  });
