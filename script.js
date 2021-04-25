677a3dd2b1fb3e6bb93698a221ae8717b6ae9193
Nataly <63913225+Nataly-li@users.noreply.github.com>
2021-04-25 15:28:29 +0300

    Сделала проверку на наличие данных

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-try {
-  let username = window.location.toString().split("=")[1];
-
-  fetch(`https://api.github.com/users/${username}`)
-    .then((res) => res.json())
-    .then((json) => {
-      let nameLink = document.createElement("a");
-      nameLink.id = "link";
-      nameLink.href = json.html_url;
-      nameLink.title = "Link";
-      nameLink.innerHTML = json.name;
-      document.body.appendChild(nameLink);
-
-      if (!json.name) {
-        nameLink.innerHTML = "Natalie";
-      }
-
-      let divBio = document.createElement("div");
-      divBio.id = "bio";
-      if (json.bio === null) {
-        divBio.innerHTML = "Student of WebHero School";
-      } else {
-        divBio.innerHTML = "Every cloud has a silver lining.";
-      }
-
-      document.body.appendChild(divBio);
-      let img = new Image(250, 250);
-      img.src = json.avatar_url;
-      document.body.appendChild(img);
-    });
-} catch {
-  document.body.innerHTML = "Информация о пользователе не найдена";
+let username = window.location.toString().split("=")[1];
+console.log(username);
+if (username === undefined) {
+  document.body.innerHTML = "Nataly-li";
 }
+
+fetch(`https://api.github.com/users/${username}`)
+  .then((res) => res.json())
+  .then((json) => {
+    if (!username) {
+      alert("Информация о пользователе недоступна.");
+    }
+
+    let nameLink = document.createElement("a");
+    nameLink.id = "link";
+    nameLink.href = json.html_url;
+    nameLink.title = "Link";
+    nameLink.innerHTML = json.name;
+    document.body.appendChild(nameLink);
+
+    if (!json.name) {
+      nameLink.innerHTML = "Natalie";
+    }
+
+    let divBio = document.createElement("div");
+    divBio.id = "bio";
+    if (json.bio) {
+      divBio.innerHTML = json.bio;
+    } else {
+      divBio.innerHTML = "Student of WebHero School";
+    }
+
+    document.body.appendChild(divBio);
+    let img = new Image(250, 250);
+    img.src = json.avatar_url;
+    document.body.appendChild(img);
+  });
